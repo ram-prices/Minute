939a8422f0215d543a54a3ddfdd777f9185ca028
ram-prices <celestriad@pm.me>
2026-03-22 18:01:16 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/',
+    base: '/Minute/',
     plugins: [
       react(), 
       tailwindcss(),
